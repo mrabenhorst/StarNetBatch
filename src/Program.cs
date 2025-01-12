212e276132f5cecffc86e88db90dbf7166d450c5
Kivak <littlewolfee@gmail.com>
2025-01-11 19:17:13 -0700

    Restructure source files

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,13 @@
-namespace StarNet2Batch {
-    internal static class Program {
+namespace StarNet2Batch.src
+{
+    internal static class Program
+    {
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main() {
+        static void Main()
+        {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
